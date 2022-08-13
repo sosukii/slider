9fc2f701c7a95c3e9960df586807b15bffff3b7e
Svetlana Trukhina <sweta4ca@mail.ru>
2022-08-13 11:27:57 +0700

    fix: add adaptive to slides, remove border-radius

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const images = document.querySelectorAll('.slider-line img')
-const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
+// const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
 const sliderLine_width = +(window.getComputedStyle(document.querySelector('.slider-line')).width.replace('px', ''))
 const sliderLine = document.querySelector('.slider-line')
 // const BASE_LEFT = +(window.getComputedStyle(document.querySelector('.slider-line')).left.replace('px', ''))
@@ -9,11 +9,13 @@ const sliderLine_gap = +(window.getComputedStyle(document.querySelector('.slider
 let offset = 0;
 
 document.querySelector('.arrow-to-right').addEventListener('click', () => {
+    const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
     offset = offset + (slide_width+sliderLine_gap)
 
     // sliderLine_style__left = sliderLine_style__left - (slide_width+sliderLine_gap) 
 console.log(offset);
 console.log((slide_width*images.length)/2);
+console.log('slide width: ', slide_width);
     sliderLine.style.left = -offset + 'px' 
     if(offset > (slide_width*images.length)/2) {
         console.log('if worked!');
@@ -22,6 +24,7 @@ console.log((slide_width*images.length)/2);
     }
 })
 document.querySelector('.arrow-to-left').addEventListener('click', () => {
+    const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
     offset = offset - (slide_width+sliderLine_gap)
     // sliderLine_style__left = sliderLine_style__left + (slide_width+sliderLine_gap)
     console.log(-offset);
