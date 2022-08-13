1040efdd9b769151d17ff6a50712f5d0b09f4186
Svetlana Trukhina <sweta4ca@mail.ru>
2022-08-13 15:12:11 +0700

    fix: add dots active visability

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const images = document.querySelectorAll('.slider-line img')
-// const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
-const sliderLine_width = +(window.getComputedStyle(document.querySelector('.slider-line')).width.replace('px', ''))
+const dots = document.querySelectorAll('.dots-wrapper div')
+const array_dots = Array.from(dots)
+const default_activeDot = document.querySelector('.dot-active')
 const sliderLine = document.querySelector('.slider-line')
-// const BASE_LEFT = +(window.getComputedStyle(document.querySelector('.slider-line')).left.replace('px', ''))
-let sliderLine_style__left =  +(window.getComputedStyle(document.querySelector('.slider-line')).left.replace('px', ''))
 const sliderLine_gap = +(window.getComputedStyle(document.querySelector('.slider-line')).gap.replace('px', ''))
 
 let offset = 0;
 
 document.querySelector('.arrow-to-right').addEventListener('click', () => {
     const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
+    const active_dot = document.querySelector('.dot-active')
+    console.log(active_dot);
     offset = offset + (slide_width+sliderLine_gap)
 
-    // sliderLine_style__left = sliderLine_style__left - (slide_width+sliderLine_gap) 
-console.log(offset);
-console.log((slide_width*images.length)/2);
-console.log('slide width: ', slide_width);
     sliderLine.style.left = -offset + 'px' 
     if(offset > (slide_width*images.length)/2) {
-        console.log('if worked!');
         sliderLine.style.left = 0
         offset = 0
+        default_activeDot.classList.add('dot-active')
     }
+
+    const active_dot_index = array_dots.indexOf(active_dot) //3
+
+    active_dot.classList.remove('dot-active')
+    const nextDot = array_dots[active_dot_index+1]
+    nextDot.classList.add('dot-active')
+
 })
 document.querySelector('.arrow-to-left').addEventListener('click', () => {
     const slide_width = +(window.getComputedStyle(document.querySelector('.slider-line img')).width.replace('px', ''))
+    const active_dot = document.querySelector('.dot-active')
     offset = offset - (slide_width+sliderLine_gap)
-    // sliderLine_style__left = sliderLine_style__left + (slide_width+sliderLine_gap)
-    console.log(-offset);
-    console.log((slide_width*images.length)/2);
+
     if(-offset > (slide_width*images.length)/2 ){
         sliderLine.style.left = 0
         offset = 0
+        default_activeDot.classList.add('dot-active')
     }
     sliderLine.style.left = -offset + 'px'
+
+    const active_dot_index = array_dots.indexOf(active_dot)
+    active_dot.classList.remove('dot-active')
+    const nextDot = array_dots[active_dot_index-1]
+    nextDot.classList.add('dot-active')
 })
